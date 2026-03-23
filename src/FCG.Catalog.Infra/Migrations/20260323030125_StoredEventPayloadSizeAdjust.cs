23ab4bf07e0d72faea2b498ed8240e37ee6b7adb
thon18valentim <othonvvalentim@gmail.com>
2026-03-23 00:02:06 -0300

    creating new migrations

---
+++ src/FCG.Catalog.Infra/Migrations/20260323030125_StoredEventPayloadSizeAdjust.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Catalog.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class StoredEventPayloadSizeAdjust : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EventType",
+                table: "StoredEvents",
+                type: "varchar(MAX)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AggregateType",
+                table: "StoredEvents",
+                type: "varchar(MAX)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EventType",
+                table: "StoredEvents",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(MAX)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AggregateType",
+                table: "StoredEvents",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(MAX)");
+        }
+    }
+}
